54f08a09f4906cb91ddca55b3f30d54c56b91020
Hudson Israel <hudson.israel716@gmail.com>
2024-05-11 17:34:58 -0300

    Ajustes tela de login

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,24 +1,33 @@
 import { LandPlot } from 'lucide-react'
 import { Outlet } from 'react-router-dom'
 
+import { ThemeToggle } from '@/components/theme/theme-toggle'
+
+import { CarouselPlugin } from './carousel-sign'
+
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2 antialiased">
+    <div className="min-h-screen grid-cols-2 antialiased lg:grid lg:grid-cols-2">
+      <div className="relative flex flex-col items-center justify-center">
+        <Outlet />
+      </div>
       <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-        <div className="flex items-center gap-3 text-lg font-medium text-foreground">
-          <LandPlot className="h-5 w-5" />
-          <span className="font-semibold">Sportix</span>
+        <div className="flex items-center justify-between gap-3 text-lg font-medium text-foreground">
+          <div className="flex items-center gap-3">
+            <LandPlot className="h-5 w-5" />
+            <span className="font-semibold">Sportix</span>
+          </div>
+          <div>
+            <ThemeToggle />
+          </div>
         </div>
-        <div className="flex h-[800px] w-[800px]">
+        {/* <div className="flex h-[800px] w-[800px] ">
           <video src="./public/video-teste.mp4" autoPlay muted loop />
-        </div>
+        </div> */}
+        <CarouselPlugin />
         <footer className="text-sm">
           &copy; Sportix - {new Date().getFullYear()}
         </footer>
-      </div>
-
-      <div className="relative flex flex-col items-center justify-center">
-        <Outlet />
       </div>
     </div>
   )
--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -60,9 +60,6 @@ export function SignUp() {
     <>
       <Helmet title="Cadastro" />
       <div className="p-8">
-        <Button asChild className="absolute right-8 top-8">
-          <Link to="/sign-in">Fazer Login</Link>
-        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -140,6 +137,10 @@ export function SignUp() {
 
             <Button disabled={isSubmitting} className="w-full" type="submit">
               Finalizar Cadastro
+            </Button>
+
+            <Button asChild className="w-full" variant="secondary">
+              <Link to="/sign-in">Fazer Login</Link>
             </Button>
 
             <p className="px-6 text-center text-sm leading-relaxed text-muted-foreground">
